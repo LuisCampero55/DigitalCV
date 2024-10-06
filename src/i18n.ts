182b26f4604119ed23a0c913214ca80405e82eb9
Luis Daniel Campero Granados <201822578@tese.edu.mx>
2024-10-05 18:46:52 -0600

    He modificado el link

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import i18nBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://DigitalCV.app'
+const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://digitalcv-luiscampero.vercel.app'
 
 i18n.use(i18nBackend).use(initReactI18next).init({
     lng: 'en',
